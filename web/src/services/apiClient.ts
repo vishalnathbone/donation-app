289d35e8d9e214ec7b039e09f375827c3c4d5dc7
vishal nathbone <vishalnathbone89@gmail.com>
2026-09-07 21:47:18 +0530

    Update apiClient.ts

--- web/src/services/apiClient.ts
+++ web/src/services/apiClient.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import { safeStorage } from '../utils/storage';
 
-const API_BASE_URL = '';
+//const API_BASE_URL = '';
+const API_BASE_URL = import.meta.env.VITE_API_URL || '';
+
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
